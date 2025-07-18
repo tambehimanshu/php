f2678aeacdcaeeb7510c65ffbee51359f18fca4a
tambehimanshu <tambehimanshu18@gmail.com>
2025-07-18 22:00:47 +0530

    variables in php

---
+++ basic.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Website</title>
+</head>
+<body>
+    <p>Website of PHP </p>
+    <?php
+    $a = 23;
+    $b = 5; 
+
+    echo $a + $b;
+    
+    ?>
+</body>
+</html>
+
+
