06824ad399fd14983888ba940c018908f92b0c25
tambehimanshu <tambehimanshu18@gmail.com>
2025-07-18 21:55:52 +0530

    php code

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <div>
+        
+        <?php
+        echo "hello php this is my first php code";
+        ?>
+    </div>
+</body>
+</html>
